f89c38ca392edba626bfd97ac16f95e7fc0f1b24
wenjinzhang <44008219+wenjinzhang@users.noreply.github.com>
2021-12-02 17:40:12 -0500

    do lab3 bonus part

---
+++ example/main.c
@@ -0,0 +1,10 @@
+// Once the installation succeded:
+// Compilation: sslittle-na-sstrix-gcc -x c++ main.c -o main
+// Execution: ../build/simplesim-3.0/sim-safe main
+#include <stdio.h>
+int main() {
+	int i;
+	for(i=0; i<10000000;i++){
+	}
+	printf("Hello world!");
+}
